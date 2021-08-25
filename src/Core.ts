e24ba9d7b326bea707addd4e312ec2017864ae3b
Franklin Waller <fr.l.waller@gmail.com>
2021-08-25 13:44:42 +0200

    WIP: Adding modules

---
+++ src/Core.ts
@@ -0,0 +1,47 @@
+import Balance from "./Balance";
+import { ClaimResult } from "./ClaimResult";
+import DataRequest from "./DataRequest";
+import { Outcome } from "./Outcome";
+import Provider from "./Provider";
+import { StakeResult } from "./StakeResult";
+
+export interface IJobWalker {
+    requests: Map<string, DataRequest>;
+    processingIds: Map<string, Promise<void>>;
+
+    addNewDataRequest(request: DataRequest): Promise<void>;
+    walkRequest(input: DataRequest): Promise<void>;
+    walkAllRequests(): Promise<void>;
+    stopWalker(): Promise<void>;
+    startWalker(): void;
+}
+
+export interface IProviderRegistry {
+    providers: Provider[];
+    activeProviders: string[];
+
+    getProviderById(id: string): Provider | undefined;
+    init(): Promise<void>;
+    getBalanceInfo(providerId: string): Promise<Balance>;
+    listenForRequests(onRequests: (requests: DataRequest[], providerId: string) => void): void;
+    getDataRequestById(providerId: string, requestId: string): Promise<DataRequest | undefined>;
+    stake(providerId: string, request: DataRequest, answer: Outcome): Promise<StakeResult>;
+    finalize(providerId: string, request: DataRequest): Promise<boolean>;
+    claim(providerId: string, request: DataRequest): Promise<ClaimResult>;
+}
+
+export interface IDatabase {
+    startDatabase(dbPath: string, dbName: string): Promise<any>;
+    createDocument(tableKey: string, id: string, obj: object): Promise<void>;
+    deleteDocument(tableKey: string, id: string): Promise<void>;
+    findDocumentById<T>(tableKey: string, id: string): Promise<T | null>;
+    getAllFromTable<T>(tableKey: string): Promise<T[]>;
+    createOrUpdateDocument(tableKey: string, id: string, obj: object): Promise<void>;
+}
+
+export interface ILogger {
+    debug(message: string): void;
+    info(message: string): void;
+    warn(message: string): void;
+    error(message: string): void;
+}
---
+++ src/Module.ts
@@ -0,0 +1,49 @@
+import { IJobWalker, IProviderRegistry, IDatabase, ILogger } from './Core';
+import DataRequest from './DataRequest';
+import { EnvArgs } from './Provider';
+
+export interface ModuleDependencies {
+    database: IDatabase;
+    providerRegistry: IProviderRegistry;
+    jobWalker: IJobWalker;
+    logger: ILogger;
+}
+
+export default class Module {
+    public name: string = 'module';
+
+    constructor(public config: EnvArgs, public dependencies: ModuleDependencies) {}
+
+    /**
+     * Initializes the module.
+     *
+     * @return {Promise<void>}
+     * @memberof Module
+     */
+    async init(): Promise<void> {
+        return Promise.resolve();
+    }
+
+    /**
+     * Calls the module with the request from the job walker
+     * Each module can do whatever they want with the request
+     *
+     * @param {Request} request
+     * @param {(request: Request) => void} next
+     * @return {Promise<void>}
+     * @memberof Module
+     */
+    async call(request: DataRequest, next: (request: DataRequest) => void): Promise<void> {
+        next(request);
+    }
+
+    /**
+     * Calls this each x amount of time. Used for sheduling requests or updating logs.
+     *
+     * @return {Promise<void>}
+     * @memberof Module
+     */
+    async tick(timeSinceLastTick: number): Promise<void> {
+        return Promise.resolve();
+    }
+}
